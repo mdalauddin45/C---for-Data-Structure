8dd59709613919e316b8b3d3d05c828c8388e46e
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-20 23:56:13 +0600

    string input

---
+++ Module 01/String Input.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char s[100];
+    // cin>>s; // without space input
+    cin.getline(s,100); // with space input
+    cout<<s;
+    return 0;
+}
