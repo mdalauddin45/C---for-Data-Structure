cfd41347505eb549ac27fc185d954359e19d82d3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-20 23:20:20 +0600

    switch case

---
+++ Module 01/Switch_case.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    switch (a)
+    {
+    case 1:
+        cout<<"One";
+        break;
+    case 2:
+        cout<<"Two";
+        break;
+    case 3:
+        cout<<"Threee";
+        break;
+    case 4:
+        cout<<"Four";
+        break;
+    case 5:
+        cout<<"Five";
+        break;
+    
+    default:
+        cout<<"I dont know";
+        break;
+    }
+    return 0;
+}
