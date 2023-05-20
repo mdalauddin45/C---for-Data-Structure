8b2f9c38f71f1aefe9e71737ddc4e77a1b45559c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-20 23:37:34 +0600

    with build in fucntion use to max and min

---
+++ Module 01/min_max_without_function.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<algorithm>//min and max er jnn
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int mn=min(a,b);
+    int mx=max(a,b);
+    cout<<"The minimun value is "<<mn<<endl<<"The maximum value is "<<mx<<endl;
+    return 0;
+}
