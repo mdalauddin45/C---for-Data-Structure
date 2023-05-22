2d306ea791ca7799c821490c35434cbbf0151da3
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-22 22:00:37 +0600

    dynamic data

--- Module 02/Dynamic_data.cpp
+++ Module 02/Dynamic_data.cpp
@@ -3,7 +3,12 @@ using namespace std;
 int main(){
     int *a= new int; //syntax of dynamic memory allocate
     *a=10; //derepearence value 
-    cout<<a<<endl; // address return korby ex: 0x1061750
-    cout<<*a<<endl; // value return korby ex: 10
+    cout<<"Address of a "<<a<<endl; // address return korby ex: 0x1061750
+    cout<<"Value of a "<<*a<<endl; // value return korby ex: 10
+
+    float *b= new float;
+    *b=10.30;
+    cout<<"Address of b "<<b<<endl;
+    cout<<"value of b "<<*b<<endl;
     return 0;
 }
