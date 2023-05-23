faffe24ddc4dcc80f002716c653bdb56eb082f9f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-24 00:46:45 +0600

    area of circle

---
+++ Codeforces problem solved/E_Area_of_a_Circle.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double a,area,r=3.141592653;
+    cin>>a;
+    area=r*(a*a);
+    cout << fixed << setprecision(9)<<area<<endl;
+    // printf("%.9lf",area);
+    return 0;
+}
