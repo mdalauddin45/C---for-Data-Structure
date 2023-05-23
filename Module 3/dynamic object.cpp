be73dcc6217fd66a427c52201eb2f1c97d2529c9
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-23 22:57:14 +0600

    dynamic object

---
+++ Module 3/dynamic object.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Students
+{
+    public:
+    char name[100];
+    int roll;
+    int cls;
+    char sec;
+    Students(int r,int s,int c,char*n)
+    {
+        roll=r;
+        sec=s;
+        cls=c;
+        strcpy(name,n);
+    }
+};
+
+int main(){
+    // with out constructor declear a dynamic object 
+    //   Students*mynul=new Students;
+    char name[100]="Mynul Alam";
+     // with constructor declear a dynamic object 
+     // dereperace kory function k print korty hoi example:  cout<<(*mynul).roll<<endl;
+        Students*mynul=new Students(31,'A',7,name);
+        cout<<(*mynul).roll<<endl;
+        cout<<mynul->cls<<endl;
+//    cout<<mynul.roll<<endl<<mynul.sec<<endl<<mynul.cls<<endl<<mynul.name<<endl;
+    return 0;
+}
