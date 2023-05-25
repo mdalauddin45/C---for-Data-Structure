254aaeaf1ba14d39a3b44a96e8599891d17587ef
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-25 21:42:57 +0600

    classs object

---
+++ Mid Term/Class and objects.cpp
@@ -0,0 +1,14 @@
+// Example
+// Create an object of MyClass
+#include<bits/stdc++.h>
+using namespace std;
+class MyClass {     
+  public:           
+    int n;        
+};
+int main() {
+  MyClass obj; 
+  obj.n = 15; 
+  cout<< obj.n << "\n";
+  return 0;
+}
