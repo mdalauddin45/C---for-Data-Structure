84f6bc783dc8bc4d9821002de38e3ed1fdc5e04f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-27 23:21:06 +0600

    stirng

---
+++ Module 4/String.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // char a[100];
+    // string a; // input ala uddin output ala
+    // cin>>a;
+    // cout<<a<<endl;
+    // cout<<a.size()<<endl;
+    string s1="ala";
+    string s2="uddin";
+    cout<<s1+s2<<endl;
+    return 0;
+}
