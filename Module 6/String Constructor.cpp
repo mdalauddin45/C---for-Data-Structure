f480e6a1e4097401d2e9ec0cb93aa5893caccf94
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-28 21:29:35 +0600

    string constructor

---
+++ Module 6/String Constructor.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // string s="Hello world";  1
+    // string s("Hello world"); 2
+    // string s("Hello world",4); 3
+    // string a="hello world";
+    // string s(a,4); output: o world
+    // string s("hello world",4); output: hell
+    string s(26,'A'); output: AAAAAAAAAAAAAAAAAAAAAAAAAA
+    cout<<s<<endl;
+    return 0;
+}
