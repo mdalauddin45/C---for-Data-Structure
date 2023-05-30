8b3eef2a1322f51d92e56990c77de5862cdc4a8a
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-30 22:09:47 +0600

    mx objects

---
+++ Module 7/Get Max or Min Object Depend on Marks.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student
+{
+    public:
+    string name;
+    int roll;
+    int marks;
+};
+int main()
+{
+    Student a[3];
+    for(int i=0;i<3;i++)
+    {
+        getline(cin,a[i].name);
+        cin>>a[i].roll>>a[i].marks;
+        cin.ignore();
+    }
+    Student mx;
+    mx.marks=INT_MIN;
+    for(int i=0;i<3;i++){
+        if(a[i].marks>mx.marks){
+            mx=a[i];
+        }
+    }
+    cout<<mx.name<<endl<<mx.roll<<endl<<mx.marks<<endl;
+    return 0;
+}
