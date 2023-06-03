705e8a4503859225e291d44f617de12fc88abd4f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-03 22:34:26 +0600

    Sort_it Solved

--- Final/Sort_It.cpp
+++ Final/Sort_It.cpp
@@ -11,7 +11,7 @@ class Student
         int eng_marks;
         int total_marks;
 };
-bool cmpByMarks(Student a, Student b)
+bool cmp(Student a, Student b)
 {
     if (a.total_marks == b.total_marks) return a.id < b.id;
     return a.total_marks > b.total_marks;
@@ -21,21 +21,16 @@ int main()
     int n;
     cin >> n;
     cin.ignore();
-    Student std[n];
+    Student a[n];
     for(int i=0; i<n; i++)
     {
-        cin >> std[i].nm;
-        cin >> std[i].cls;
-        cin >> std[i].s;
-        cin >> std[i].id;
-        cin >> std[i].math_marks;
-        cin >> std[i].eng_marks;
-        std[i].total_marks = std[i].math_marks + std[i].eng_marks;
+        cin >> a[i].nm>> a[i].cls>> a[i].s>> a[i].id>> a[i].math_marks>> a[i].eng_marks;
+        a[i].total_marks = a[i].math_marks + a[i].eng_marks;
     }
-    sort(std, std+n, cmpByMarks);
+    sort(a, a+n, cmp);
     for(int i=0; i<n; i++)
     {
-         cout << std[i].nm << " " << std[i].cls << " " << std[i].s << " " << std[i].id << " " << std[i].math_marks << " " << std[i].eng_marks << endl;
+         cout << a[i].nm << " " << a[i].cls << " " << a[i].s << " " << a[i].id << " " << a[i].math_marks << " " << a[i].eng_marks << endl;
     }
     return 0;
 }
