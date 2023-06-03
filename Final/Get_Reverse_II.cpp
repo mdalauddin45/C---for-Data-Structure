2a2fc79ce8b616a7cddb169a3653c3329348dac5
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-06-03 22:08:59 +0600

    get reverse ll

--- Final/Get_Reverse_II.cpp
+++ Final/Get_Reverse_II.cpp
@@ -3,34 +3,31 @@ using namespace std;
 class Student
 {
     public:
-    string name;
-    int cls; 
-    char s;
-    int id;
+        string nm;
+        int cls;
+        char s;
+        int id;
 };
-bool cmp(Student a,Student b)
-{
-    if(a.id<b.id) return true;
-    else return false;
-}
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     cin.ignore();
-    Student a[n];
-    for(int i=0;i<n;i++)
+    Student std[n];
+    for(int i=0; i<n; i++)
     {
-        // getline(cin,a[i].name);
-        cin>>a[i].name;
-        cin>>a[i].cls>>a[i].s>>a[i].id;
-        cin.ignore();
+        cin >> std[i].nm;
+        cin >> std[i].cls;
+        cin >> std[i].s;
+        cin >> std[i].id;
     }
-    sort(a,a+n,cmp);
-    for(int i=0;i<n;i++){
-       
-        cout<<a[i].name<<" "<<a[i].cls<<" "<<a[i].s<<" "<<a[i].id<<endl;
+    for (int i=0; i<n/2; i++) {
+        swap(std[i].id, std[n-1-i].id);
     }
-     
+    for(int i=0; i<n; i++)
+    {
+         cout << std[i].nm << " " << std[i].cls << " " << std[i].s << " " << std[i].id << endl;
+         
+    }
     return 0;
 }
