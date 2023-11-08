7ad2edd7b5dda37c53559484999b3afc19d2796f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-08 10:21:36 +0600

    4A - Watermelon

---
+++ Codeforces problem solved/A_Watermelon.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a;
+    cin>>a;
+    if(a%2==0 && a>= 4) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
