8e1b63255e766ddb61d09b39daa4c03803d091c8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-22 20:33:57 +0600

    Cricket World Cup Qualifier

---
+++ Codechef/Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int tp;
+    cin >> tp;
+
+    if (tp >= 12) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+	return 0;
+}
